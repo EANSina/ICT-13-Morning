6f6cdd96e267f4127e9a69f036068ea9e3deb5a6
Sina Ean <sinaeean@gmail.com>
2025-06-05 00:15:55 +0700

    update Assignment

---
+++ Assignment/allTes.cpp
@@ -0,0 +1,10 @@
+#include <iostram>
+using namespace std;
+int main () {
+	Employee emp[5];
+	// Assume you already input data into emp[0] to emp[4]
+	emp[0].searchById(emp, 5);
+
+	
+	return (0);
+}
